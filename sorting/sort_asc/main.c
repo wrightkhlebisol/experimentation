5cc8c729b1babded44836da8db65b043d57247d1
wrightkhlebisol <calebogundiya@gmail.com>
2023-09-20 23:24:51 +0100

    sort asc

---
+++ sorting/sort_asc/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 1000
+
+int main(void)
+{
+    int arr[MAX];
+
+    for (int i = 0; i < MAX; i++)
+    {
+        arr[i] = arc4random_uniform(MAX + 1);
+    }
+
+    //sorting the array
+    for (int i = 0; i < MAX; i++)
+    {
+        for (int j = i + 1; j < MAX; j++)
+        {
+            if (arr[j] < arr[i])
+            {
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        } 
+    }
+
+    for (int i = 0; i < MAX; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    return (0);
+}
